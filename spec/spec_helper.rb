e6c17c44fd9814e90b4f4f11e4f9f3dfa191f8cc
Peter Suschlik <ps@neopoly.de>
2013-09-20 10:02:38 +0200

    Use Struct as backend for HashObject.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,18 +14,8 @@ require 'bound'
 #
 class HashObject
   def self.new(hash)
-    build_class(hash.keys).new(hash)
-  end
-
-  def self.build_class(attributes)
-    Class.new do
-      attr_accessor(*attributes)
-
-      def initialize(hash)
-        hash.each do |attr, value|
-          public_send "#{attr}=", value
-        end
-      end
-    end
+    attributes  = hash.keys
+    values      = hash.values
+    Struct.new(*attributes).new(*values)
   end
 end
