0a1da94964f972aa5bdb7e8464a6f6a13adbece9
Jakob Holderbaum <jh@neopoly.de>
2013-10-02 08:34:43 +0200

    Reimplement HashObject without Struct

--- spec/support/hash_object.rb
+++ spec/support/hash_object.rb
@@ -7,16 +7,29 @@
 # OpenStruct.new(:id => 2).name # => nil
 # HashObject.new(:id => 2).name # => NoMethodError
 #
+# We also can not us struct, because it breaks on 1.9.3
+# with ?-postifxed attributes. 
+#
+# Hence this bare-metal implementation
+#
 class HashObject
   def self.new(hash)
     attributes  = hash.keys
-    values      = hash.values
-    if attributes.empty?
-      Class.new
-    else
-      mapped_values = values.map { |v| map_value(v) }
-      Struct.new(*attributes).new(*mapped_values)
+
+    cls = Class.new
+
+    attributes.each do |attr|
+      define_accessor cls, attr
     end
+
+    instance = cls.new
+
+    attributes.each do |attr| 
+      value = map_value(hash[attr])
+      assign_value instance, attr, value
+    end
+
+    instance
   end
 
   def self.map_value(value)
@@ -28,5 +41,21 @@ class HashObject
     else
       value
     end
+  end
+
+  def self.define_accessor(cls, attribute)
+    cls.send :define_method, :"#{attribute}=" do |a|
+      @suffix_attributes ||= {}
+      @suffix_attributes[attribute] = a
+    end
+
+    cls.send :define_method, :"#{attribute}" do
+      @suffix_attributes ||= {}
+      @suffix_attributes[attribute]
+    end
+  end
+
+  def self.assign_value(instance, attr, value)
+    instance.send :"#{attr}=", value
   end
 end
